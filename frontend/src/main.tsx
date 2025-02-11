a1bb98a8c611babe6f61aabb8c331940e144bc93
kumalpatel <kumal.patel21@gmail.com>
2025-02-11 13:46:43 -0500

    style: changed custom css variables and removed default dark mode

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,9 +3,6 @@ import { createRoot } from "react-dom/client";
 import "@/index.css";
 import App from "@/App.tsx";
 
-// set default theme to dark
-document.documentElement.classList.add("dark");
-
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
